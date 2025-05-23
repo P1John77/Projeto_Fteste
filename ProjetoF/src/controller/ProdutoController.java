35a2ea731e4826df8f7864f544a64c1499afe42f
John <johncarlosd42@gmail.com>
2025-05-23 20:01:39 -0300

    Update ProdutoController.java

--- ProjetoF/src/controller/ProdutoController.java
+++ ProjetoF/src/controller/ProdutoController.java
@@ -3,6 +3,8 @@ package controller;
 import model.Produto;
 import service.ProdutoService;
 
+import java.util.List;
+
 public class ProdutoController {
 
     private ProdutoService produtoService = new ProdutoService();
@@ -11,5 +13,27 @@ public class ProdutoController {
         return produtoService.salvar(produto);
     }
 
+    public List<Produto> listarTodos() {
+        return produtoService.getProdutos();
+    }
 
+    public Produto buscarPorId(Integer id) {
+        return produtoService.buscarPorId(id);
+    }
+
+    public boolean remover(Integer id) {
+        return produtoService.removerProduto(id);
+    }
+
+    public void adicionarAoCarrinho(Produto produto) {
+        produtoService.adicionarAoCarrinho(produto);
+    }
+
+    public List<Produto> visualizarCarrinho() {
+        return produtoService.getCarrinho();
+    }
+
+    public void limparCarrinho() {
+        produtoService.limparCarrinho();
+    }
 }
